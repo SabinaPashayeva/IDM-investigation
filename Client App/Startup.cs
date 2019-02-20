0db34d7f9b29b38077f6afa0e97582cb468e3a5b
maryna-t <43456771+maryna-t@users.noreply.github.com>
2019-02-20 16:15:31 +0200

    startup

--- Client App/Startup.cs
+++ Client App/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -24,15 +25,31 @@ namespace Client_App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddMvc();
+
+            // turned off the JWT claim type mapping to allow well-known claims(e.g. ‘sub’ and ‘idp’) to flow through unmolested:
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //AddAuthentication adds the authentication services to DI
+            services.AddAuthentication(options =>
+                {
+                    // using a cookie to locally sign-in the user 
+                    options.DefaultScheme = "Cookies";
+                    // using the OpenID Connect protocol
+                    options.DefaultChallengeScheme = "oidc";
+                })
+                // add the handler that can process cookies
+                .AddCookie("Cookies")
+                //configure the handler that perform the OpenID Connect protocol
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ClientId = "mvc";
+                    options.SaveTokens = true;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,16 +65,10 @@ namespace Client_App
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
